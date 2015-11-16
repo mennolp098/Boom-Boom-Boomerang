46849eb27f23ef5867f133ba6985ffe0ce468760
Yornie Westink <jinxie88@live.nl>
2015-11-16 09:15:54 +0100

    Enemies,Start

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour 
+{
+	public int _moveSpeed =1;
+
+	private TouchDetector2D _touchDetectorEnemy;
+
+
+	void Start()
+	{
+		_touchDetectorEnemy = GetComponent<TouchDetector2D>();
+
+		_touchDetectorEnemy.TouchStarted += OnTouchStarted;
+		_touchDetectorEnemy.OnTouch += OnTouchStay;
+		_touchDetectorEnemy.TouchEnded += OnTouchExit;
+	}
+	
+	
+	public virtual void Update()
+	{
+		Move ();
+	}
+
+	//Overridable enemy movement
+	public virtual void Move()
+	{
+	}
+	
+	void OnTouchStarted(GameObject other, Vector2 dir)
+	{
+		if (dir == Vector2.down) 
+		{
+			if(other.transform.tag == Tags.PLAYER)
+			{
+				Death();
+			}
+		}
+
+	}
+
+	void Idle()
+	{
+		_moveSpeed = 0;
+	}
+
+	void OnTouchStarted()
+	{
+		
+	}
+
+	void OnTouchStay()
+	{
+
+	}
+
+	void OnTouchExit()
+	{
+
+	}
+
+
+	void Death()
+	{
+
+	}
+}
---
+++ Assets/Scripts/Enemy/EnemyGreen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyGreen : Enemy 
+{
+	void Start() 
+	{
+
+	}
+
+	public override void Update()
+	{
+		base.Update();
+	}
+	public override void Move ()
+	{
+		base.Move ();
+		transform.Translate (Vector2.left * _moveSpeed * Time.deltaTime);
+	}
+
+
+
+	void OnTouchStay()
+	{
+	
+	}
+
+	void OnTouchExit()
+	{
+
+	}
+
+}
